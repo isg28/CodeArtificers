fff5041e0d5bd0224d61abb1858dc99b5e99aa80
Danica Galang <babykuma@comcast.net>
2023-10-08 19:18:59 -0700

    Added Scheduling App Files

---
+++ scheduling-app/src/test/java/com/codeartificers/schedulingapp/SchedulingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeartificers.schedulingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
