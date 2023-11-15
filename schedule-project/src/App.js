4c013dc6946347841219094a963f5b05e050343f
Mansoor-Ali-0701 <mansoorali0701@gmail.com>
2023-11-14 22:49:21 -0800

    Created a link on scheduling, login, and calendar button with seprate paths

--- schedule-project/src/App.js
+++ schedule-project/src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import React from 'react';
 import AppBar from './components/AppBar';
 import User from './components/User';
-import Calendar from './components/Calendar';
+
+const headingStyle = {
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  margin: '20px 0', 
+  textAlign: 'center', 
+  color: '#333', 
+};
 
 
 function App() {
   return (
     <div className="App">
       <AppBar />
+      <div style={headingStyle}>
+      Schedule Your Availabilities Now
+      </div>
       <User />
-      <Calendar />
-
     </div>
   );
 }
