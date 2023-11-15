538a89053459236548fa835dff86cf4de685c120
Mansoor-Ali-0701 <mansoorali0701@gmail.com>
2023-11-14 22:55:42 -0800

    Created pages folder where all the pages files will be kept

---
+++ schedule-project/src/pages/CalendarPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calendar from '../components/Calendar';
+import AppBar from '../components/AppBar';
+function CalendarPage() {
+  return (
+    <div>
+    <AppBar />
+    <Calendar />
+    </div>
+  )
+}
+
+export default CalendarPage
---
+++ schedule-project/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '../components/AppBar';
+
+function LoginPage() {
+  return (
+    
+    <div className="App">
+      <AppBar />
+      <div>LoginPage</div>
+      </div>
+  )
+}
+
+export default LoginPage
