502d77c1a4c55f3e16e3d5a0df4af35516357bea
Mansoor-Ali-0701 <mansoorali0701@gmail.com>
2023-11-14 22:53:17 -0800

    Routed the path corresponding to each path

--- schedule-project/src/index.js
+++ schedule-project/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import CalendarPage from './pages/CalendarPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/custom-login" element={<LoginPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        {/* ... etc. */}
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
