0acc4a119acd966512e23de26347fe3da5325edf
Mansoor-Ali-0701 <mansoorali0701@gmail.com>
2023-11-26 19:57:51 -0800

    Made create meeting button and prompt

--- schedule-project/src/components/Calendar.jsx
+++ schedule-project/src/components/Calendar.jsx
@@ -11,8 +11,25 @@ function Calendar(){
     /*const [userId, setUserId] = useState(null);*/
     //hardcoded user so you can post events, will fix this when we get user authentication working
     const userId = 1;
-
-
+    const [showCreateMeetingForm, setShowCreateMeetingForm] = useState(false);
+    const [newMeeting, setNewMeeting] = useState({
+      date: "",
+      startTime: "",
+      endTime: "",
+      location: "",
+      meeting_Description: "",
+    });
+    const handleCreateMeeting = async () => {
+      console.log("Meeting created:", newMeeting);
+      setNewMeeting({
+            date: "",
+            startTime: "",
+            endTime: "",
+            location: "",
+            meeting_Description: "",
+          });
+          setShowCreateMeetingForm(false);
+        };
     const handleDateClick = async (info) => {
       const title = prompt('Enter event title: ');
         const isAllDay = window.confirm('Is this an all-day event?');
@@ -34,7 +51,7 @@ function Calendar(){
             return
           }
         }
-
+    
         const newEvent = {
           title: title,
           userId: userId,
@@ -68,17 +85,31 @@ function Calendar(){
         //alert('Invalid input. Please make sure to enter a title and valid start/emd time.')
       //}
     };
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setNewMeeting((prevMeeting) => ({
+        ...prevMeeting,
+        [name]: value,
+      }));
+    };  
 
     const isValidInputTimeValue = (time) => {
       const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]([APMapm]{2})?$/;
       return regex.test(time);
     };
 
+    const toggleCreateMeetingForm = () => {
+      console.log("Toggling create meeting form");
+      setShowCreateMeetingForm(!showCreateMeetingForm);
+    };
 
     return(
       <div>
         <h1> </h1>
         <Box sx={{ display: 'flex', justifyContent: 'flex-end','& button': { m: 1, backgroundColor: 'black' } }}>
+        <Button variant="contained" size="large" onClick={toggleCreateMeetingForm}>
+        Create Meeting
+        </Button>   
       <div>
         <Button variant="contained" size="large">
           Common TimeSlots
@@ -99,7 +130,60 @@ function Calendar(){
           events = {events}
           dateClick={handleDateClick}
           />
-          
+          {showCreateMeetingForm && (
+        // Renders the pop up box to create a meeting
+      
+        <div
+        style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        backgroundColor: "#3087CF",
+        padding: "20px",
+        zIndex: 1000,
+          }}
+        >
+          <label style={{ color: 'white' }}>Date:</label>
+          <input
+          type="date"
+          name="date"
+          value={newMeeting.date}
+          onChange={handleInputChange}
+          />
+          <label style={{ color: 'white' }}>Start Time:</label>
+          <input
+          type="time"
+          name="startTime"
+          value={newMeeting.startTime}
+          onChange={handleInputChange}
+          />
+          <label style={{ color: 'white' }}>End Time:</label>
+          <input
+          type="time"
+          name="endTime"
+          value={newMeeting.endTime}
+          onChange={handleInputChange}
+          />
+          <label style={{ color: 'white' }}>Location:</label>
+          <input
+          type="text"
+          name="location"
+          value={newMeeting.location}
+          onChange={handleInputChange}
+          />
+          <label style={{ color: 'white' }}>Meeting Description:</label>
+          <textarea
+          name="meeting_Description"
+          value={newMeeting.meeting_Description}
+          onChange={handleInputChange}
+          ></textarea>
+          <div>
+          <button onClick={handleCreateMeeting} >
+            Create Meeting</button>
+            </div>
+          </div>
+        )}
       </div>
       );
     } 
