30836484060a0185a9b1fe9bc04e50709571f344
Phorealzman <brandon.nguyen421@gmail.com>
2023-12-07 17:04:16 -0800

    All of the fetch api operations for user profile (tysm isabelgit push)

--- schedule-project/src/components/Profile.jsx
+++ schedule-project/src/components/Profile.jsx
@@ -5,8 +5,8 @@ import './Profile.css'
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import { deepOrange, deepPurple } from '@mui/material/colors';
-
-
+import jwt from 'jsonwebtoken';
+import { useNavigate} from 'react-router-dom'
 const UserProfile = () => {
   // Initial user data
   const initialUserData = {
@@ -16,25 +16,128 @@ const UserProfile = () => {
     email: '',
     dob: '',
   };
-
-  // State to track user data
+  const [user_id, setUserId] = useState(null);
+  const [isEditMode, setIsEditMode] = useState(false);
   const [userData, setUserData] = useState(initialUserData);
-
-  // State to track which field is being edited
   const [editingField, setEditingField] = useState(null);
+  const [fieldErrors, setFieldErrors] = useState({});
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [confirmUsername, setConfirmUsername] = useState('');
 
-  // Function to handle field editing
+  const toggleEditMode = () => {
+    setIsEditMode((prevMode) => !prevMode);
+
+    setFieldErrors({});
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const decodedToken = jwt.decode(token);
+        const user_id = decodedToken.user_id;
+
+        setUserId(user_id);
+      } catch (error) {
+        console.error("Error decoding token: ", error);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = getToken();
+        if (!user_id) {
+          console.error('User ID is undefined');
+        }
+
+        const response = await fetch(`http://localhost:8080/api/user/${user_id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const userDataFromServer = await response.json();
+          setUserData(userDataFromServer);
+        } else {
+          console.error('Failed to fetch user data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error during fetch:', error);
+      }
+    };
+    if (user_id) {
+      fetchUserData();
+    }
+  }, [user_id, isEditMode]); 
+
+  
   const handleFieldEdit = (fieldName) => {
     setEditingField(fieldName);
   };
 
-  // Function to handle changes in input fields
   const handleInputChange = (e) => {
+    const { name, value } = e.target;
     setUserData({
       ...userData,
-      [editingField]: e.target.value,
+      [name]: value,
     });
+
+    // Validate the field
+    validateField(name, value);
   };
+
+
+  const validateField = (name, value) => {
+    const nameRegex = /^[A-Za-z\s-]+$/;
+    const emailRegex = /\S+@\S+/;
+    const usernameRegex = /^[A-Za-z0-9_]+$/;
+    const dobRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+    setValidationErrors([]);
+
+    let errorMessage = '';
+
+    switch (name) {
+      case 'firstName':
+      case 'lastName':
+        if (!nameRegex.test(value)) {
+          validationErrors.push("Invalid First Name or Last Name. Only letters are allowed.");
+        }
+        break;
+      case 'email':
+        if (!emailRegex.test(value)) {
+          validationErrors.push("Invalid Email Address. It must contain an @ symbol.");
+        }
+        break;
+      case 'username':
+        if (!usernameRegex.test(value)) {
+          validationErrors.push("Invalid Username. Only letters, numbers, and underscores are allowed.");
+        }
+        break;
+      case 'dob':
+        if (!dobRegex.test(value)) {
+          validationErrors.push("Invalid Date of Birth. It must be in yyyy-mm-dd format.");
+        }
+        break;
+      default:
+        break;
+      }
+
+      setFieldErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: errorMessage,
+      }));
+      if (errorMessage) {
+        setValidationErrors((prevErrors) => [...prevErrors, errorMessage]);
+      }
+    };
+
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -56,46 +159,104 @@ const UserProfile = () => {
   };
 
   const changeAvatar = () => {
-    // Trigger the file input when the avatar is clicked
     document.getElementById('avatarInput').click();
   };
 
+  const saveChanges = async () => {
+    // Validate all fields before attempting to save changes
+    validateAllFields();
 
-  // Function to save changes and end editing
-  const saveChanges = () => {
-    setEditingField(null);
-    // Add logic to save changes to the server or perform other actions
+    // Check if any validation errors exist
+    if (validationErrors.length > 0) {
+      alert('Please correct the following errors before saving changes:\n\n' + validationErrors.join('\n'));
+      // Clear validation errors after displaying the alert
+      setValidationErrors([]);
+      return;
+    }
+
+    try {
+      const token = getToken();
+      const response = await fetch(`http://localhost:8080/api/user/${user_id}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(userData),
+      });
+  
+      if (response.ok) {
+        console.log('User profile updated successfully');
+      } else {
+        console.error('Failed to update user data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error during update:', error);
+    } finally {
+      setEditingField(null);
+      toggleEditMode();
+    }
   };
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try{
-        const response = await fetch("http://localhost/api/user/{user_id}",{
-          method: "GET",
-          headers:{
+  const validateAllFields = () => {
+    // Validate all fields and update the state
+    for (const [name, value] of Object.entries(userData)) {
+      validateField(name, value);
+    }
+  };
+
+  const handleDeleteButtonClick = () => {
+    setShowDeleteConfirmation(true);
+  };
+
+  const navigate = useNavigate();  
+
+  const handleDeleteConfirm = async () => {
+    try {
+      if (confirmUsername === userData.username) {
+        const token = getToken();
+        const response = await fetch(`http://localhost:8080/api/user/${user_id}`, {
+          method: 'DELETE',
+          headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ${yourAuthToken}',
+            Authorization: `Bearer ${token}`,
           },
         });
-        if(response.ok){
-          const userDataFromServer = await response.json();
-          setUserData(userDataFromServer);
-        } else{
-          console.error('Failed to fetch user data:', response.statusText);
+
+        if (response.ok) {
+          navigate('/');
+        } else {
+          console.error('Failed to delete user:', response.statusText);
         }
-      }catch(error){
-        console.error('Error during fetch:', error);
+      } else {
+        alert('Username does not match. Deletion canceled.');
       }
-    };
-    fetchUserData();
-  }, []);
+    } catch (error) {
+      console.error('Error during deletion:', error);
+    } finally {
+      setShowDeleteConfirmation(false);
+    }
+  };
+
+  const handleDeleteCancel = () => {
+    setShowDeleteConfirmation(false);
+  };
+  
+  const getToken = () => {
+    const token = localStorage.getItem("token");
+    if(!token){
+      console.error('Token not found. User not authenticated.');
+    }
+    return token;
+  }
+
 
   return (
     <div className="profile-container">
       <h2>User Profile</h2>
 
       <div className="avatar-container" onClick={changeAvatar}>
-      {userData.profilePicture ? (
+        {userData.profilePicture ? (
           <img className="profile-picture" src={userData.profilePicture} alt="Profile Icon" />
         ) : (
           <img className="profile-picture" src={profile_icon} alt="Placeholder Icon" />
@@ -110,62 +271,82 @@ const UserProfile = () => {
         style={{ display: 'none' }}
       />
 
-
       <label htmlFor="firstName">First Name:</label>
       <input
         type="text"
         id="firstName"
-        value={editingField === 'firstName' ? userData.firstName : userData.firstName}
+        name="firstName"
+        value={userData.firstName}
         onClick={() => handleFieldEdit('firstName')}
         onChange={handleInputChange}
-        disabled={!editingField || editingField !== 'firstName'}
+        disabled={!isEditMode}
       />
 
       <label htmlFor="lastName">Last Name:</label>
       <input
         type="text"
         id="lastName"
-        value={editingField === 'lastName' ? userData.lastName : userData.lastName}
+        name = 'lastName'
+        value={userData.lastName}
         onClick={() => handleFieldEdit('lastName')}
         onChange={handleInputChange}
-        disabled={!editingField || editingField !== 'lastName'}
+        disabled={!isEditMode}
       />
 
       <label htmlFor="username">Username:</label>
       <input
         type="text"
         id="username"
-        value={editingField === 'username' ? userData.username : userData.username}
+        name = 'username'
+        value={userData.username}
         onClick={() => handleFieldEdit('username')}
         onChange={handleInputChange}
-        disabled={!editingField || editingField !== 'username'}
+        disabled={!isEditMode}
       />
 
       <label htmlFor="email">Email:</label>
       <input
         type="email"
         id="email"
-        value={editingField === 'email' ? userData.email : userData.email}
+        name = 'email'
+        value={userData.email}
         onClick={() => handleFieldEdit('email')}
         onChange={handleInputChange}
-        disabled={!editingField || editingField !== 'email'}
+        disabled={!isEditMode}
       />
 
       <label htmlFor="dob">Date of Birth:</label>
       <input
         type="text"
         id="dob"
-        value={editingField === 'dob' ? userData.dob : userData.dob}
+        name = 'dob'
+        value={userData.dob}
         onClick={() => handleFieldEdit('dob')}
         onChange={handleInputChange}
-        disabled={!editingField || editingField !== 'dob'}
+        disabled={!isEditMode}
       />
 
-      {editingField && (
-        <button onClick={saveChanges}>Save Changes</button>
+      {isEditMode && <button onClick={saveChanges}>Save Changes</button>}
+      {!isEditMode && <button onClick={toggleEditMode}>Edit</button>}
+
+      <button onClick={handleDeleteButtonClick}>Delete Account</button>
+
+      {showDeleteConfirmation && (
+        <div className="delete-confirmation">
+        <p>Are you sure you want to delete your account?</p>
+        <p>Please enter your username to confirm:</p>
+        <input
+          type="text"
+          value={confirmUsername}
+          onChange={(e) => setConfirmUsername(e.target.value)}
+        />
+        <button onClick={handleDeleteConfirm}>Confirm</button>
+        <button onClick={handleDeleteCancel}>Cancel</button>
+        </div>
       )}
     </div>
   );
 };
 
 export default UserProfile;
+
